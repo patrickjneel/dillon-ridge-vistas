ca99c8a68685cf8bd4d82ac79f73980ff6ef7040
Pat Neel <patrick.neel@colorado.edu>
2018-03-19 22:45:41 -0600

    added image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Dillon Ridge Vistas</h1>
         </header>
+          <img src={require('./dillon.svg')} alt="logo" />
         <footer>
           Stuff goes here
         </footer>
