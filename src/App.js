2d3c7fe9bd3c80155538f2fbf2442102753f059a
Pat Neel <patrick.neel@colorado.edu>
2018-03-21 10:26:05 -0600

    routes fixed

--- src/App.js
+++ src/App.js
@@ -9,28 +9,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="top">
-        </div>
-        <header className="app-footer">
-        </header>
-        <div className="all-info">
-          <div className="welcome-area">
-            <div className="welcome-image"></div>
-          </div>
-          <div className="info-area">
-            <h1 className="app-title">Dillon Ridge VISTAS</h1>
-            <h1 className="app-title">Leasing Now for the Summer of 2018</h1>
-            <div className="button-links">
-              <NavArea />
-              <Route exact path="/viewfloorplans" component={ViewFloorPlans} key="ViewFloorPlans" />
-              <Route exact path="/contact" component={Contact} key="Contact" />
-            </div>      
-          </div>
-        </div>
-        <footer className="app-footer">
-        </footer>
-        <div className="top">
-        </div>
+        <div className="top"></div>
+          <Route exact path="/" component={NavArea} key="NavArea" />
+          <Route exact path="/viewfloorplans" component={ViewFloorPlans} key="ViewFloorPlans" />
+          <Route exact path="/contact" component={Contact} key="Contact" />
+        <div className="top"></div>
       </div>
     );
   }
--- src/components/NavArea.js
+++ src/components/NavArea.js
@@ -4,8 +4,23 @@ import { NavLink } from 'react-router-dom';
 const NavArea = () => {
   return (
     <div>
-      <NavLink className="nav-links" to="/viewfloorPlans">View Floor Plans</NavLink>
-      <NavLink className="nav-links" to='/contact'>Contact</NavLink>
+      <header className="app-footer">
+      </header>
+      <div className="all-info">
+        <div className="welcome-area">
+        <div className="welcome-image"></div>
+        </div>
+        <div className="info-area">
+          <h1 className="app-title">Dillon Ridge VISTAS</h1>
+          <h1 className="app-title">Leasing Now for the Summer of 2018</h1>
+          <div className="button-links">
+          <NavLink className="nav-links" to="/viewfloorPlans">View Floor Plans</NavLink>
+          <NavLink className="nav-links" to='/contact'>Contact</NavLink>      
+        </div>
+      </div>
+      <footer className="app-footer">
+      </footer>
+      </div>
     </div>
   )
 }
