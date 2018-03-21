213d7d8f89c4ea3dc0ab9d7d7fb79ee95943d3d4
Pat Neel <patrick.neel@colorado.edu>
2018-03-21 16:23:09 -0600

    floor plan css and contact css

--- src/components/ViewFloorPlans.js
+++ src/components/ViewFloorPlans.js
@@ -4,14 +4,14 @@ import './view-floor-plan.css';
 const ViewFloorPlans = () => {
   return (
     <div className="floor-plans">
-      <h1>Available Floor Plans</h1>
+      <h1 className="floorplan-header">Available Floor Plans</h1>
       <div className="model">
-        <h2>2 Bedroom - 1 Bathroom</h2>
-        <img src={require('../assets/second.svg')} />
+        <h2 className="floor-title">2 Bedroom - 1 Bathroom</h2>
+        <img className="floor-img" src={require('../assets/second.svg')} />
       </div>
       <div className="model">
-        <h2>2 Bedroom - 2 Bathroom</h2>
-        <img src={require('../assets/third.svg')} />
+        <h2 className="floor-title">2 Bedroom - 2 Bathroom</h2>
+        <img className="floor-img" src={require('../assets/third.svg')} />
       </div>
     </div>
   );
