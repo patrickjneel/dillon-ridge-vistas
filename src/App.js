99052a69bca02573fbf7d3a7f41b108aa8714871
Pat Neel <patrick.neel@colorado.edu>
2018-03-22 16:40:14 -0600

    nav-bar section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import ViewFloorPlans from './components/ViewFloorPlans';
 import Contact from './components/Contact';
 import NavArea from './components/NavArea';
+import NavBar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="top"></div>
+          <Route path="/" component={NavBar} key="NavBar" />
           <Route exact path="/" component={NavArea} key="NavArea" />
           <Route exact path="/viewfloorplans" component={ViewFloorPlans} key="ViewFloorPlans" />
           <Route exact path="/contact" component={Contact} key="Contact" />
